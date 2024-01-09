76bc2a9abd3ba25747e9b8478f01eb5478bfc125
AdiN2009 <88230987+AdiN2009@users.noreply.github.com>
2024-01-10 10:31:50 +1100

    Add files via upload

---
+++ c130.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import csv
+
+df = pd.read_csv("Data_merged.csv")
+print(df.shape)
+
+print(df.columns)
+
+
+df.drop(columns=['Unnamed: 0','Luminosity', 'Star_name_y', 'Distance_y', 'Mass_y', 'Radius_y'])
+columns_to_clean = ['id', 'Star_name_x', 'Distance_x', 'Mass_x', 'Radius_x']
+df[columns_to_clean] = df[columns_to_clean].fillna(0)
+
+
+print(df.columns)
+print(df.describe())
+print(df.info())
+print(df.dtypes)
+
+
+df.to_csv('final_data.csv',index=True, index_label="id")
